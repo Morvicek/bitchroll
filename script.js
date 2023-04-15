5c6afba204e1f513faab4cba3ed10ff65b79b824
Morvicek <65585242+Morvicek@users.noreply.github.com>
2023-04-15 20:49:45 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('cropy').innerText = `© 1918 - ${new Date().getFullYear()} ~ Morvicek`;
+
+})
+
+const x = [
+  'Lída',
+  'Natka',
+  'Laura',
+  'Kája',
+  'Ella',
+  'Róza',
+  'Mája',
+  'Lucka (Letiště) '
+];
+
+const l = [
+  'Včera večer v parku...',
+  'baf',
+  'Tak sem tady, co byly tvoje další dvě přání?',
+];
+
+$("#roll").click(function(){
+
+  let random = Math.ceil((Math.random()*x.length));
+  let es = x[random];
+  $("#left").html(es);
+
+  let randome = Math.ceil((Math.random()*l.length));
+  let ese = l[randome];
+  $("#right").html(ese);
+});
+
+function set(){
+  let random = Math.ceil((Math.random()*x.length-1));
+  let es = x[random];
+  $("#left").html(es);
+
+    let randome = Math.ceil((Math.random()*l.length-1));
+  let ese = l[randome];
+  $("#right").html(ese);
+}
+const delay = (delayInms) => {
+  return new Promise(resolve => setTimeout(resolve, delayInms));
+}
+
+async function roll(){
+set()
+}
